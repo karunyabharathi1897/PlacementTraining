7482e7970680fb0fe108e9c23b75b218d468a84c
karunyabharathi1897 <karunyabharathi1897@gmail.com>
2017-12-20 16:34:09 +0530

    Create Rotate an Array.c

---
+++ Trainer1/Rotate an Array.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int n,k;
+	scanf("%d",&n);
+	scanf("\n%d",&k);
+	for(int i=0;i<k;i++)
+	{
+		int t=a[0];
+		for(int j=i+1;j<n;j++)
+		{
+			a[i]=a[i+]
+		}
+	}
+	
+	return 0;
+}
