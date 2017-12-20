8c3e13a861664a7b6ed79b5fcfb8806e21e71bdd
karunyabharathi1897 <karunyabharathi1897@gmail.com>
2017-12-20 19:23:29 +0530

    Update Rotate an Array.c

--- Trainer1/Rotate an Array.c
+++ Trainer1/Rotate an Array.c
@@ -3,16 +3,25 @@
 int main(void) {
 	// your code goes here
 	int n,k;
+	int a[10];
 	scanf("%d",&n);
 	scanf("\n%d",&k);
+	for(int i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
 	for(int i=0;i<k;i++)
 	{
 		int t=a[0];
-		for(int j=i+1;j<n;j++)
+		for(int j=0;j<n;j++)
 		{
-			a[i]=a[i+]
+			a[j]=a[j+1];
 		}
+		a[n-1]=t;
 	}
-	
+	for(int i=0;i<n;i++)
+	{
+		printf("%d ",a[i]);
+	}
 	return 0;
 }
